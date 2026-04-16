3a3c8011bdd4a1199d4c64eb960bfb1fc22f22cf
simon-laroche <simon.laroche@ind-philippeville.be>
2026-04-16 13:35:13 +0200

    ok

--- interro/ec-20260416.py
+++ interro/ec-20260416.py
@@ -1,5 +1,15 @@
 menu_liste = ["quitter le menu", "afficher le catalogue", "enregistrez un noouveau client", "ajouter un article au panier du client", "afficher la fiche d'un client" ]
+def affiche_menu():
+    """Affiche le menu principal."""
+    print("\nMenu principal :")
+    print("0 : quitter le programme")
+    print("1 : afficher le catalogue")
+    print("2 : enregistrez un nouveau client")
+    print("3 : ajouter un artticle au panier du client")
+    print("4 : afficher la fiche d'un client")
 
+
+   
 
 catalogue = {
     'pain_blanc' :15,
@@ -16,8 +26,34 @@ clients = {
             'pain_blanc' : 1,
             'pain_complet' : 1,
             'pistolet' : 3
-
-        }
+            }
     }
 }
-while 
+choix = input ("quel est votre choix : ")
+if choix == "0":
+    print("au revoir")
+
+
+
+
+
+
+
+print("nom du produit et la quantité")
+for ref, produit in catalogue.items():  
+    print(produit["nom"], produit["quantité"])
+
+email = input("email : ")
+
+if email in clients :
+    print("client déjà exixstant")
+else : 
+    nom = input("nom : ")
+    prenom = input("prenom: ")
+
+    clients[email] = {
+        "nom" : nom,
+        "prenom": prenom,
+        "panier" : {}
+    }
+    print ("client ajouté ")
